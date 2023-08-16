51ea6c2bdec564630d1b83dbec966a518972d823
Alex212202 <94471440+Alex212202@users.noreply.github.com>
2023-08-16 17:03:27 +0300

    FlipACoin

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    coin_result: "head",
+  };
+
+  flipACoin() {
+    const random = Math.floor(Math.random() * 2) + 1;
+    console.log(random);
+
+    if (random == 1) {
+      //head
+
+      this.setState({
+        coin_result: "head",
+      });
+    } else {
+      //tail
+
+      this.setState({
+        coin_result: "tail",
+      });
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.state.coin_result == "head" ? (
+          <Image
+            source={require("./assets/1_grivna_gerb.jpg")}
+            style={{
+              width: 150,
+              height: 150,
+            }}
+          />
+        ) : (
+          <Image
+            source={require("./assets/1_grivna_number.jpg")}
+            style={{
+              width: 150,
+              height: 150,
+            }}
+          />
+        )}
+
+        <TouchableOpacity
+          onPress={() => {
+            this.flipACoin();
+          }}
+          style={styles.button}
+        >
+          <Text>Flip A Coin</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#4fe8f0",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#f1c40f",
+    padding: 20,
+    margin: 10,
+    borderRadius: 4,
   },
 });
